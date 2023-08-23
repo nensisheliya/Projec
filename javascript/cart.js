f5e2952c15a65ee6fa6f8473d1a9ae4081a47e09
nensi <nensisheliya04@gmail.com>
2023-08-23 13:52:51 +0530

    cart

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,3 +1,37 @@
 import navbar from '../components/nav.js';
 
 document.getElementById('navbar').innerHTML =  navbar()
+import navbar from "../componets/nav.js";
+document.getElementById("navbar").innerHTML = navbar();
+
+
+const ui = (data) => {
+    data.map((prodcut) => {
+      if (prodcut.catagary == "gift plants") {
+        let img1 = document.createElement("img")
+        img1.src = prodcut.img1
+        let title = document.createElement("p")
+        title.innerHTML = prodcut.title
+        let selling_price = document.createElement("p")
+        selling_price.innerHTML = prodcut.selling_price
+        let orginal_price = document.createElement("p")
+        orginal_price.innerHTML = prodcut.orginal_price
+        let btn = document.createElement("button")
+        btn.innerHTML = "buy now"
+        let div = document.createElement("div")
+        div.append(title, selling_price, orginal_price, btn)
+        let div2 = document.createElement("div")
+        div2.append(img1,div)
+        document.getElementById("cart").append(div2)
+      }
+    })
+  };
+  
+  let get = async () => {
+    let res = await fetch("http://localhost:3000/cart");
+    let data = await res.json();
+    ui(data);
+    console.log(data)
+  };
+  
+  get();
--- javascript/product.js
+++ javascript/product.js
@@ -2,3 +2,31 @@ import navbar from '../components/nav.js';
 
 document.getElementById('navbar').innerHTML =  navbar()
 
+const display=(data)=>{
+    data.map((product)=>{
+        let img=document.createElement("img");
+        img.src=product.image;
+        let title=document.createElement("h2");
+        title.innerHTML=product.title;
+        let price=document.createElement("h4");
+        price.innerHTML=product.price;
+        let category=document.createElement("p");
+        category.innerHTML=product.category;
+        let rate=document.createElement("p");
+        rate.innerHTML=product.rating.rate;
+        let btn=document.createElement("button");
+        btn.innerHTML="buy Now";
+        
+        let div = document.createElement("div")
+        div.append(img,title,price,category,rate,btn)
+        document.getElementById("box2").append(div)
+    })
+}
+
+const get=()=>{
+    fetch("http://localhost:3000/product")
+    .then((response)=> response.json())
+    .then((response)=>display(response));
+}
+
+get();
